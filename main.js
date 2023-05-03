497e924775a4bee707341c65bd9db37094cf4c06
Jory Roberts <robertsjory13@gmail.com>
2023-05-03 01:11:45 -0500

    adds pseudocode for planning in main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+/*
+PSEUDOCODE:
+
+variables:
+    for game
+    for score
+    for difficultyLevel = [use array]
+
+
+function to fetch data from API {
+    fetch(data from API)
+    Grab genre data from API
+    array method => (function to render genre data to the DOM)
+
+}
+
+function to render genre data to the DOM {
+    create.element(something)
+    create.element(something)
+    create(element(something)
+
+  
+}
+
+*/
